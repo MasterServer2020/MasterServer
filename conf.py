e52d999ad1bcd2a3787ef6630acc537b351d18b6
MasterServer2020 <65570713+MasterServer2020@users.noreply.github.com>
2020-06-07 19:27:07 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -83,7 +83,7 @@ pygments_style = 'emacs'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_materialdesign_theme'
+html_theme = 'default'
 html_logo='_static/logo.jpg'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
